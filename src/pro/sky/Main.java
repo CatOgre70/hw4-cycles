81868b3d5360553a199d3a32622147ce78b2dd2f
Vasily Demin <vasily.demin@gmail.com>
2022-04-04 18:14:28 +0300

    First three mandatory tasks of HW4

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -4,7 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Task 1. Numbers
+        int i = 1;
+        while(i <= 10) {
+            System.out.printf(i + " ");
+            i++;
+        }
+        System.out.printf("\n");
 
+        i = 10;
+        while(i > 0) {
+            System.out.printf(i + " ");
+            i--;
+        }
+        System.out.printf("\n");
+
+        // Task 2. Fridays
+        int fistFridayDay = 3;
+
+        System.out.println("В этом месяце первая пятница - это " + fistFridayDay + "-е число");
+        i = fistFridayDay;
+        do {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+            i += 7;
+        } while(i <= 31);
+
+        // Task 3. Astronomy application
+        int currentYear = 2077; // Cyberpunk forever!
+        int minus200Years = currentYear - 200, plus100Years = currentYear + 100;
+
+        System.out.println("За последние 200 лет комета приближалась к Солнцу и Земле ");
+        for(i = 0; i <= currentYear; i += 79){
+            if( i >= minus200Years) {
+                System.out.println("в " + i + "-ом году");
+            }
+        }
+        System.out.println("В ближайшие 100 лет у нас с вами будет шанс увидеть комету");
+        for(i = 0; i <= plus100Years; i += 79){
+            if(i > currentYear) {
+                System.out.println("в " + i + "-ом году");
+            }
+        }
 
     }
 }
